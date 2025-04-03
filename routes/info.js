923b405f5b103e71096c37078ad39e3885c1e1f1
Herbal7ea <me@pracprog.dev>
2025-04-03 17:19:22 +0900

    Works for me • Moved to MySql2 • Added sample health file.

--- routes/info.js
+++ routes/info.js
@@ -1,7 +1,7 @@
 // This file is intended to be used for the "/api/info/*" route
 const express = require('express')
 const router = express.Router()
-const mysql = require('mysql')
+const mysql = require('mysql2')
 const conn = require('../dbconnecter')
 
 router.get('/channels/:channel', (req, res) => {
@@ -20,7 +20,22 @@ router.get('/users', async (req, res) => {
     res.status(500).send("service unavailable, no database set up")
 })
 
+router.get('/health', async (req, res) => {
+    // show all users
+    //TODO: make an optional choice to limit how many users queried
 
+    conn.query(
+        'SELECT * FROM child',
+        (err, results) => {
+            if (err) {
+                res.status(500).json({ error: err.message });
+                return;
+            }
+            res.send(results);
+        }
+    );
 
+    // res.status(200).send("works on my machine")
+})
 
 module.exports = router;
